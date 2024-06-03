f8260b616a46bccc90dfc5084f7f480db5c02725
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2024-06-03 17:17:44 +0530

    Update postdata.js

--- routes/postdata.js
+++ routes/postdata.js
@@ -5,18 +5,24 @@ require('../connection');
 
 // importing model
 const RunModel = require('../models/dataschema')
-
 router.post('/postdata', async (req, res) => {
-    try {
-        let data = new RunModel(req.body)
-        //if we want to modify data we can do that here!
-        await data.save()
-        res.status(201).json({"success":data})
-    } catch (error) {
-        res.status(500).json({"Error":error})
-        console.error(error)
+  try {
+    const data = new RunModel(req.body);
+    // Modify data if needed
+    await data.save();
+    res.status(201).json({ success: data });
+  } catch (error) {
+    if (error.code === 11000 && error.keyPattern && error.keyPattern.runID) {
+      // Duplicate runID error
+      res.status(400).json({ error: 'Duplicate runID. Please choose a different ID.' });
+    } else {
+      // Other errors
+      res.status(500).json({ error: 'Internal server error' });
     }
-})
+    console.error(error);
+  }
+});
+
   
 
 
