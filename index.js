8f54a29b063c0e5eace89c8de36d7b0756a8a47d
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2024-06-03 18:02:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const hostname="0.0.0.0"
 const port = process.env.PORT || 3000;
 const app=express();
@@ -14,6 +15,17 @@ app.use(express.urlencoded({extended: true}))
 app.use(postdata)
 app.use(getdata)
 
+
+var corsOptions = {
+    origin: function (origin, callback) {
+        callback(null, true)
+      },  // Specify your origin here
+    credentials: true,  // This allows the session cookie to be sent back and forth
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
+//middleware
+app.use(cors(corsOptions))
 
 app.get("/",(req,res)=>{
     res.json({"success":"App deployed sucessfully"})
